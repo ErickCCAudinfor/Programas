dd12079a73316c88f73bbbf0663e49ec2b0ad353
ErickCCAudinfor <143091799+ErickCCAudinfor@users.noreply.github.com>
2024-03-21 12:19:42 +0100

    Compilacion Release

--- ActualizaPrecios/obj/Debug/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
+++ ActualizaPrecios/obj/Debug/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6edff6e7a6adad386b5d48221023f1f3965cafab"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93b6a657ed22b8645b27fc78cd88536a8b75563"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
