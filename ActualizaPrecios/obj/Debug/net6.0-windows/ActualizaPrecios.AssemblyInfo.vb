e7d847544a18b775594d1406dcb3ac675188176e
ErickCCAudinfor <143091799+ErickCCAudinfor@users.noreply.github.com>
2026-03-02 10:06:21 +0100

    Organización de Carpetas

--- ActualizaPrecios/obj/Debug/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
+++ ActualizaPrecios/obj/Debug/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc91db433b941129c7b6d9ba462a34fcbcdce46c"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b47e67350a75be971ae26710402a5d424ee258"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
