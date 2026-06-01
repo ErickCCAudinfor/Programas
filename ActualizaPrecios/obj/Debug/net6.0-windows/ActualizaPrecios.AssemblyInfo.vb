ba9cb35230805408272ceca9727cea966ef292c8
ErickCCAudinfor <143091799+ErickCCAudinfor@users.noreply.github.com>
2026-06-01 11:37:54 +0200

    Nuget

--- ActualizaPrecios/obj/Debug/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
+++ ActualizaPrecios/obj/Debug/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b47e67350a75be971ae26710402a5d424ee258"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ba50bf92656d599034faa3281f8a34e6d77644"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
--- ActualizaPrecios/obj/Release/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
+++ ActualizaPrecios/obj/Release/net6.0-windows/ActualizaPrecios.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc91db433b941129c7b6d9ba462a34fcbcdce46c"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ba50bf92656d599034faa3281f8a34e6d77644"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("ActualizaPrecios"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
